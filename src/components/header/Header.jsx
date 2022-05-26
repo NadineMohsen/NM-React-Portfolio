bd9a086275325948e40d3f843c31d981c97ee3d3
Nadine <nadine.mohsen@hotmail.com>
2022-05-26 16:31:43 +0100

    header style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import './Header.css'
 import CTA from './CTA'
+import NM from '../../assets/NM.png'
+import Socials from './Socials'
 function Header() {
   return (
     <header>
-      <div className="container header__container">
-        <h5>
-          Hello, I'm
-        </h5>
-        <h1>
-          Nadine Mohsen
-        </h1>
-       <h5 className="text-light">Fullstack Deveoper</h5>
+      <div className="container header-container">
+        <h3 className="text-light">
+          Welcome to my Portfolio
+        </h3>
+       <h1 className="text-light">Nadine Mohsen </h1>
+       <h3 className="text-light">Fullstack Web Deveoper</h3>
         <CTA />
+        <div className="me">
+        <img src={NM} alt="logo"></img>
+        </div>
+        <Socials />
+
+      <a href="#contact" className="scroll-down">>>>></a>
       </div>
+
     </header>
   )
 }
