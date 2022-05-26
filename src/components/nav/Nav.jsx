18272b3675ab9f5249d4ddb8c47093c6631b8069
Nadine <nadine.mohsen@hotmail.com>
2022-05-26 16:31:02 +0100

    navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {RiMacbookLine} from 'react-icons/ri'
+import {RiContactsLine} from 'react-icons/ri'
+import {useState} from 'react'
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div></div>
+    <nav>
+      <a href="#" onClick={()=> setActiveNav("#")} className={activeNav === '#' ? 'active': ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={()=> setActiveNav("#about")} className={activeNav === '#about' ? 'active': ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={()=> setActiveNav("#esperience")} className={activeNav === '#experience' ? 'active': ''}><RiMacbookLine/></a>
+      <a href="#contact" onClick={()=> setActiveNav("#contact")} className={activeNav === '#about' ? 'contact': ''}><RiContactsLine /></a>
+    </nav>
   )
 }
 
