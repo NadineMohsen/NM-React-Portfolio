92ae1a2e9a49e146321aa1ff037b8c9fec1e7cee
Nadine <nadine.mohsen@hotmail.com>
2022-05-26 16:32:02 +0100

    contact me btn

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href={CV} download className="btn">Donwnload CV</a>
-        <a href="#contact" className="btn btn-primary">Contact</a>
+        <a href="#contact" className="btn">Contact Me</a>
     </div>
   )
 }
