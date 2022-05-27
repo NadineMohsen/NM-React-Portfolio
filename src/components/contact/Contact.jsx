8e57c340ec8fcaa6f278e64146d886e6d251e19d
Nadine <nadine.mohsen@hotmail.com>
2022-05-27 18:32:06 +0100

    contact details and form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import './Contact.css'
-
+import {MdOutlineMail} from "react-icons/md"
+import {FaLinkedinIn} from "react-icons/fa"
 function Contact() {
   return (
     <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact-container">
+          <div className="contact-options">
+            <article className="contact-option">
+                 <MdOutlineMail className="contact-icons"/>
+                <h4>Email</h4>
+                <h5>nadine.mohsen@hotmail.com</h5>
+                <a href="mailto:nadine.mohsen@hotmail.com" target="_blank">Send me an email</a>
+            </article>
 
+            <article className="contact-option">
+                 <FaLinkedinIn className="contact-icons"/>
+                <h4>LinkedIn</h4>
+                <h5>Nadine Mohsen</h5>
+                <a href="https://www.linkedin.com/in/nadine-mohsen/" target="_blank">Message me on LinkedIn</a>
+            </article>
+            </div>
+
+          <form action="">
+              <input type="text" name="name" placeholder="Full name" required/>
+              <input type="email" name="email" placeholder="Email" required/>
+              <textarea name="message" rows="7" placeholder="Your Message Here" required></textarea>
+              <button type="submit" className="btn btn-primary">Send Message</button>
+          </form>
+
+       
+      </div>
     </section>
   )
 }
