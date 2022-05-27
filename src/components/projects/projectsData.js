d86eb9ca1e5f919785fcc6d343ca7be47ffd1d71
Nadine <nadine.mohsen@hotmail.com>
2022-05-27 18:30:46 +0100

    project data

---
+++ src/components/projects/projectsData.js
@@ -0,0 +1,55 @@
+import img1 from "../../assets/creatives.jpg"
+import img2 from "../../assets/crypto.jpg"
+import img3 from "../../assets/tech.jpg"
+import img4 from "../../assets/texteditor.png"
+import img5 from "../../assets/weather.png"
+import img6 from "../../assets/quiz.png"
+
+const projectData = [
+    {
+        id:1,
+        image: img1,
+        title:"Creatives Lab",
+        github:"https://github.com/BavKudhail/creative-labs",
+        demo:"https://creativelabs-app.herokuapp.com/"
+    },
+    {
+        id:2,
+        image:img2,
+        title:"CryptoCurrency Guide",
+        github:"https://github.com/JosephCurtis1999/CryptocurrencyFinder",
+        demo:"https://josephcurtis1999.github.io/CryptocurrencyFinder/"
+    },
+    {
+        id:3,
+        image: img3,
+        title:"Tech Blog",
+        github:"https://github.com/NadineMohsen/Tech-Blog",
+        demo:"https://tech-blog-nm.herokuapp.com/"
+    },
+    {
+        id:4,
+        image: img4,
+        title:"Text Editor",
+        github:"https://github.com/NadineMohsen/Text-Editor",
+        demo:"https://shrouded-savannah-65472.herokuapp.com/"
+    },
+
+    {
+        id:5,
+        image: img5,
+        title:"Weather Dashboard",
+        github:"https://github.com/NadineMohsen/Weather-Dashboard",
+        demo:"https://nadinemohsen.github.io/Weather-Dashboard/"
+    },
+    {
+        id:6,
+        image: img6,
+        title:"Code Quiz",
+        github:"https://github.com/NadineMohsen/Code-Quiz",
+        demo:"https://nadinemohsen.github.io/Code-Quiz/"
+    },
+
+]
+
+export default projectData;
